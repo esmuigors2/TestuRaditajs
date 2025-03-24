28bf7a291b6202d9842890a142b9e3d25939d8f7
esmuigors2 <144150580+esmuigors2@users.noreply.github.com>
2025-03-24 14:52:13 +0200

    corrected mistake with the outputs

--- make.workflow.sh
+++ make.workflow.sh
@@ -18,7 +18,7 @@ cat classroom.yml.env.start >> classroom.yml
 ls -v test*.sh | while read testt; do
     testname="$(grep '^##' "$testt" | sed 's@^## @@;s@:@ -@g')"
     testname_stripped="$(echo "$testname" | tr '[:upper:]' '[:lower:]' | sed 's@ā@a@g;s@č@c@g;s@ē@e@g;s@ģ@g@g;s@ī@i@g;s@ķ@k@g;s@ļ@l@g;s@ņ@n@g;s@ŗ@r@g;s@ō@o@g;s@š@s@g;s@ū@u@g;s@ž@z@g;s@[: -]@-@g;s@--@-@g;s@--@-@g')"
-    echo "        $(echo "$testname_stripped" | tr '[:lower:]' '[:upper:]')_RESULTS: "'"${{steps.'"${testname_stripped}"'.result}}"' >> classroom.yml
+    echo "        $(echo "$testname_stripped" | tr '[:lower:]' '[:upper:]')_RESULTS: "'"${{steps.'"${testname_stripped}"'.outputs.result}}"' >> classroom.yml
     echo -n "$testname_stripped," >> tmp.runners
 done
 echo "      with:" >> classroom.yml
